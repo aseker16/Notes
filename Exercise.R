f8451616644bcde689f698621969c9dc964f95f5
Ahsen  Seker <ahsenseker@h9471.eduroam-5.wlan.aalborguniversitet.dk>
2018-09-19 14:42:38 +0200

    ex 1

--- Exercise.R
+++ Exercise.R
@@ -1,4 +1,25 @@
 #' ## PART 1 Exercises
 #' Excersise I
+prodfkt = function(x){
+  p = 1
+for (i in 1:length(x)) #i stedet for 1:length(x) kan seq_along(x) også bruges, den kan lave en række
+  p= p*x[i]
+return(p)
+}
+prodfkt(c(2,3,4))
+#eller bruge 
+prod(2,3,4)
+
+fib = function(n){
+  if (n == 1){
+  return(1)
+  }
+  if (n == 2){
+    return(1)
+  }
+  return(fib(n-2)+fib(n-1))
+}
+
+fib(1:10)
 #' Exercise II
 #' Exercise III
